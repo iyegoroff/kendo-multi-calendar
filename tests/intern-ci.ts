39f3be9b4286c4c6aff810c850048b1d4e7abb32
Igor Yegorov <iegoroff@gmail.com>
2016-12-19 04:53:30 +0300

    [skip ci] disabled mobile browsers

--- tests/intern-ci.ts
+++ tests/intern-ci.ts
@@ -1,51 +1,51 @@
 export * from './intern';
 
 export const environments = [
-    {
-        browserName: 'iphone',
-        version: ['8.1', '9.2'],
-        recordVideo: false,
-        recordScreenshots: false
-    },
-    {
-        browserName: 'android',
-        version: ['4.4', '5.1'],
-        recordVideo: false,
-        recordScreenshots: false
-    },
+    // {
+    //     browserName: 'iphone',
+    //     version: ['8.1', '9.2'],
+    //     recordVideo: true,
+    //     recordScreenshots: false
+    // },
+    // {
+    //     browserName: 'android',
+    //     version: ['4.4', '5.1'],
+    //     recordVideo: true,
+    //     recordScreenshots: false
+    // },
     {
         browserName: 'chrome',
         version: ['53', '54'],
         platform: ['Windows 10'],
-        recordVideo: false,
+        recordVideo: true,
         recordScreenshots: false
     },
     {
         browserName: 'firefox',
         version: ['49', '50'],
         platform: ['Windows 10'],
-        recordVideo: false,
+        recordVideo: true,
         recordScreenshots: false
     },
     {
         browserName: 'MicrosoftEdge',
         version: ['13', '14'],
         platform: 'Windows 10',
-        recordVideo: false,
+        recordVideo: true,
         recordScreenshots: false
     },
     {
         browserName: 'internet explorer',
         version: ['10', '11'],
         platform: 'Windows 7',
-        recordVideo: false,
+        recordVideo: true,
         recordScreenshots: false
     },
     {
         browserName: 'safari',
         version: ['9', '10'],
         platform: 'OS X 10.11',
-        recordVideo: false,
+        recordVideo: true,
         recordScreenshots: false
     }
 ];
