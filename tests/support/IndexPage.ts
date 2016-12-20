42ac24ed72845c6233b2fb3c9d646e7f88938b1c
Igor Yegorov <iegoroff@gmail.com>
2016-12-20 05:50:32 +0300

    decreased delays

--- tests/support/IndexPage.ts
+++ tests/support/IndexPage.ts
@@ -5,8 +5,8 @@ import Element = require('leadfoot/Element');
 
 export class IndexPage {
 
-    private static delay = 250;
-    private static navigateDelay = 2000;
+    private static delay = 125;
+    private static navigateDelay = 1000;
 
     constructor(private remote: Command<any>, depth: kendoExt.CalendarDepth = 'month') {
         let code: Function;
